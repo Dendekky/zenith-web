b8aac7587e0e673ac1d9580a77369310202c71c3
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-01-08 16:26:53 +0100

    implement review view

--- src/views/zenithReviews.js
+++ src/views/zenithReviews.js
@@ -7,55 +7,80 @@ import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
     root: {
-      display: 'flex',
-      overflow: 'hidden',
+      height: '100vh',
+      background: `url('https://i.imgur.com/ZXBtVw7.jpg') no-repeat center center`,
     },
     center: {
-        textAlign: 'center',
+      textAlign: 'center',
+    },
+    img: {
+      borderRadius: 8,
+      height: 100,
+      // width: '50px',
+      marginLeft: '24px',
+    },
+    userInfo: {
+      textAlign: 'center',
+      // display: 'flex',
+      // paddingLeft: 10,
     },
   });
 
 
-const content = [
-    {
-      title: "Vulputate Mollis Ultricies Fermentum Parturient",
-      description:
-        "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
-      image: "https://i.imgur.com/ZXBtVw7.jpg",
-    },
-    {
-      title: "Tortor Dapibus Commodo Aenean Quam",
-      description:
-        "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-      image: "https://i.imgur.com/DCdBXcq.jpg",
-    },
-    {
-      title: "Phasellus volutpat metus",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-      image: "https://i.imgur.com/DvmN8Hx.jpg",
-    }
-  ];
+
  
 const ZenithReviews = (props) => {
     const { classes } = props;
 
+    const content = [
+      {
+        title: "Vulputate Mollis Ultricies Fermentum Parturient",
+        description:
+          "Aenean eu leo quam. Pellentesque ornare sem. Fusce dapibus, tellus ac cursus, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+        userImage: <img className={classes.img} alt='image' src={require('../assets/img/01.jpg')} />,
+        userName: "Mr Ayo Babalola",
+        userPosition: "Student(200l) At FUTA"
+      },
+      {
+        title: "Tortor Dapibus Commodo Aenean Quam",
+        description:
+          "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+        userImage: <img className={classes.img} alt='image' src={require('../assets/img/02.jpeg')} />,
+        userName: "Mr Tayo Joseph",
+        userPosition: "Student(400l) At LAUTECH"
+      },
+      {
+        title: "Phasellus volutpat metus",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+        userImage: <img className={classes.img} alt='image' src={require('../assets/img/01.jpg')} />,
+        userName: "Miss Ayoola Babalola",
+        userPosition: "Corp member"
+      }
+    ];
+
     return (
-        <div>
-        {/* <h4>Our Reviews</h4> */}
+        <div
+        className={classes.root}>
+        {/* style={{ background: `url('https://i.imgur.com/ZXBtVw7.jpg') no-repeat center center` }}> */}
+          <Typography className={classes.center} variant="h4">
+              Our Reviews
+          </Typography>
         <Slider autoplay={3000}>
             {content.map((item, index) => (
                 <div
                     key={index}
-                    style={{ background: `url('${item.image}') no-repeat center center` }}
+                    // style={{ background: `url('${item.image}') no-repeat center center` }}
                 >
-                    <div className={classes.center}>
-                    <Typography variant="h4">
-                        Our Reviews
-                    </Typography>
+                    <section className={classes.center}>
                         <h1>{item.title}</h1>
                         <p>{item.description}</p>
-                    </div>
+                    </section>
+                    <section className={classes.userInfo}>
+                        {item.userImage}
+                        <h6>{item.userName}</h6>
+                        <p>{item.userPosition}</p>
+                    </section>
                 </div>
             ))}
         </Slider>
